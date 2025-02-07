7a171bf3c4447a8a3cd7d790efaf615995d6502b
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2025-02-07 00:59:17 -0500

    Game works now, but doesn't disable extra guess upon win or loss.

--- hangman.js
+++ hangman.js
@@ -36,6 +36,7 @@ console.log('Target Word: ' + target_word)
 
 
 let errors = 0;
+let correct = 0;
 
 let slides = document.getElementsByClassName('slide');
 
@@ -44,11 +45,14 @@ for (let i=1; i < slides.length; i++){
 }
 
 
-
 // Adding eventlisteners to keys and defining the function that 
 // registers the guess that was made.
 
 var keys = document.getElementsByClassName("key");
+
+var text_display = document.getElementById('text-display');
+
+text_display.textContent = target_string;
 
 Array.from(keys).forEach(function(element){
     element.addEventListener('click', function(event){
@@ -57,17 +61,52 @@ Array.from(keys).forEach(function(element){
 
         console.log('Guessed: ' + guess);
         
-        if (target_word.includes(guess)){
-            element.style.backgroundColor = "green";
-            element.style.color = "white";
+        if (errors <= 7){
+            if (target_word.includes(guess)){
+                element.style.backgroundColor = "green";
+                element.style.color = "white";
+                
+                correct += 1;
+
+                let indices = [];
+
+                for (const [index, element] of target_word_list.entries()) {
+                    if (element == guess){
+                        indices.push(index);
+                    }
+                }
+                console.log("Indices: " + indices);
+
+                for (const index of indices){
+                    target_string_list[index] = guess;
+                }
+                for (let i = 0; i < target_string_list.length; i++){
+                    if (i == 0){
+                        text_display.textContent = target_string_list[0];
+                    }
+                    else{
+                        text_display.textContent += target_string_list[i];
+                    }
+                }  
+            }
+            else{
+                if (errors <= 7){
+                    element.style.backgroundColor = "red";
+                    element.style.color = "white";
+                    slides[errors].style.display = "none";
+                    errors += 1;
+                    slides[errors].style.display = "block";
+                }
+            }
         }
-        else{
-            element.style.backgroundColor = "red";
-            element.style.color = "white";
-            slides[errors].style.display = "none";
-            errors += 1;
-            slides[errors].style.display = "block";
+
+        if (errors == 7){
+            slides[7].style.display = "block";
+            text_display.textContent = 'GAME OVER';
         }
+
+        console.log('Errors: ' + errors);
+        console.log('Target Word Length: ' + target_word.length);
 
         element.disabled = true;
         console.log(errors);
